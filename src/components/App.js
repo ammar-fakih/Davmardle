0a213f9ce8a8bb48528aa4765a2ec111f8cc27a9
ammar1029 <ammar.fakih02@gmail.com>
2022-03-12 23:50:31 -0800

    Hardcoded Dicts

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Table from './Table';
 import Footer from './Footer';
 import OnKeyboard from './OnKeyboard';
 import Modal from './Modal';
-import dicts from "../info/Script"
+import {bigdick, smalldick} from "../info/Script"
 import papaparse from 'papaparse';
 
 class App extends React.Component {
@@ -31,14 +31,14 @@ class App extends React.Component {
     gameState: "running",
     guessedWord: 0,
     guessedLetter: 0,
-    bigDick: dicts.bigdick + dicts.smalldick,
-    smallDick: dicts.smalldick,
+    bigDick: [],
+    smallDick: [],
     buttonAttributes: [],
     bThemes: [],
   };
 
   componentDidMount = () => {
-    // this.processDicks();
+    this.processDicks();
     // this.setState({smallDick: dicts.smalldick, bigDick: dicts.bigdick})
     let bThemesCopy = [];
 
@@ -197,49 +197,61 @@ class App extends React.Component {
 
   processDicks = () => {
 
-    // this.setState({smallDick: dicts.smalldick, bigDick: dicts.bigdick})
-    let bigdick =
-      'https://raw.githubusercontent.com/ggilestro/playground/main/wordle_strategy/wordle_words_accepted.txt';
+    var index = Math.floor(Math.random() * smalldick.length);
 
-    let smalldick =
-      'https://raw.githubusercontent.com/ggilestro/playground/main/wordle_strategy/wordle_words.txt';
-
-    let t = this;
-
-    papaparse.parse(smalldick, {
-      download: true,
-      complete: async function (results) {
-        let arr = results.data;
-
-        for (var i = 0; i < arr.length; ++i) {
-          arr[i] = arr[i][0];
-        }
-
-        var index = Math.floor(Math.random() * arr.length);
-
-        t.setState({
-          bigDick: arr,
-          smallDick: arr,
-          targetWord: arr[index],
-        });
-        console.log(arr[index]);
-      },
+    this.setState({
+      bigDick: bigdick + smalldick,
+      smallDick: smalldick,
+      targetWord: smalldick[index],
     });
+    console.log(smalldick[index]);
+    console.log(smalldick)
+    console.log(bigdick)
 
-    papaparse.parse(bigdick, {
-      download: true,
-      complete: async function (results) {
-        let arr = results.data;
 
-        for (var i = 0; i < arr.length; ++i) {
-          arr[i] = arr[i][0];
-        }
 
-        t.setState({
-          bigDick: t.state.bigDick + arr + ['ammar'] + ['david'],
-        });
-      },
-    });
+    // let bigdick =
+    //   'https://raw.githubusercontent.com/ggilestro/playground/main/wordle_strategy/wordle_words_accepted.txt';
+
+    // let smalldick =
+    //   'https://raw.githubusercontent.com/ggilestro/playground/main/wordle_strategy/wordle_words.txt';
+
+    // let t = this;
+
+    // papaparse.parse(smalldick, {
+    //   download: true,
+    //   complete: async function (results) {
+    //     let arr = results.data;
+
+    //     for (var i = 0; i < arr.length; ++i) {
+    //       arr[i] = arr[i][0];
+    //     }
+
+    //     var index = Math.floor(Math.random() * arr.length);
+
+      //   t.setState({
+      //     bigDick: arr,
+      //     smallDick: arr,
+      //     targetWord: arr[index],
+      //   });
+      //   console.log(arr[index]);
+      // },
+    // });
+
+    // papaparse.parse(bigdick, {
+    //   download: true,
+    //   complete: async function (results) {
+    //     let arr = results.data;
+
+    //     for (var i = 0; i < arr.length; ++i) {
+    //       arr[i] = arr[i][0];
+    //     }
+
+    //     t.setState({
+    //       bigDick: t.state.bigDick + arr + ['ammar'] + ['david'],
+    //     });
+    //   },
+    // });
   };
 
   render() {
